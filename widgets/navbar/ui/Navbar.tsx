59f9ecac76a7b0188533cad9b5602b4c46451ebb
Walker-19 <walkerabraham48@gmail.com>
2026-08-02 15:01:46 +0200

    fix: route param & fav icon

--- widgets/navbar/ui/Navbar.tsx
+++ widgets/navbar/ui/Navbar.tsx
@@ -194,9 +194,6 @@ export default function Navbar() {
                       <div className="navbar-dropdown-sep" />
                       {!isAdmin && (
                         <>
-                          <Link href="/profil/parametres" className="navbar-dropdown-item navbar-dropdown-item--nav" onClick={() => setUserMenuOpen(false)} role="menuitem">
-                            <span className="navbar-dropdown-item-label"><i className="fa-solid fa-gear" /> Paramètres et confidentialité</span>
-                          </Link>
                           <Link href="/contact" className="navbar-dropdown-item navbar-dropdown-item--nav" onClick={() => setUserMenuOpen(false)} role="menuitem">
                             <span className="navbar-dropdown-item-label"><i className="fa-solid fa-circle-question" /> Aide et assistance</span>
                             <i className="fa-solid fa-chevron-right" />
